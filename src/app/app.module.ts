ba799a754ad15da6ce2f27df06b957284fc33c9c
Afzal <afzalmlakdawala@gmail.com>
2016-11-21 19:13:34 +0530

    router added and productlist html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { routes } from './app.router';
 import { AppComponent } from './app.component';
 import { ProductlistComponent } from './productlist/productlist.component';
 import { ProductdetailsComponent } from './productdetails/productdetails.component';
@@ -16,7 +16,8 @@ import { ProductdetailsComponent } from './productdetails/productdetails.compone
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routes
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.router.ts
@@ -0,0 +1,14 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { ProductlistComponent } from './productlist/productlist.component';
+import { ProductdetailsComponent } from './productdetails/productdetails.component';
+
+export const router: Routes = [
+  {path: '', redirectTo: 'products', pathMatch: 'full'},
+  {path: 'products', component: ProductlistComponent},
+  {path: 'products/:id', component: ProductdetailsComponent}
+];
+
+export const routes: ModuleWithProviders = RouterModule.forRoot(router);
